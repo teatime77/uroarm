db6c193c6303e85cb97b07dc46245b2026ef5f81
teatime77 <teatime77@live.jp>
2022-05-05 09:41:10 +0900

    util.pyを追加。

---
+++ util.py
@@ -0,0 +1,13 @@
+class Glb:
+    def __init__(self):
+        self.regX = None
+        self.regY = None
+        self.inferX = None
+        self.inferY = None
+        self.prdX = None
+        self.prdY = None
+
+theGlb = Glb()
+
+def getGlb():
+    return theGlb
