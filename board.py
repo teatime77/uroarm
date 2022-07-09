56ace1f57b4ab536a57615a34147a488afa0a0c4
teatime77 <teatime77@live.jp>
2022-07-09 11:32:28 +0900

    ChArUcoボードのXYZ軸の表示を削除

--- board.py
+++ board.py
@@ -8,58 +8,13 @@
 import time
 import cv2
 from cv2 import aruco
-import os
-import numpy as np
-
-import PySimpleGUI as sg
 import cv2
-import subprocess
 
 from util import write_params, read_params
 from camera import initCamera, closeCamera, getCameraFrame
 
-def draw_axis(frame, camera_matrix, dist_coeffs, board, verbose=True):
-    corners, ids, rejected_points = cv2.aruco.detectMarkers(frame, dictionary)
-    if corners is None or ids is None:
-        return None
-    if len(corners) != len(ids) or len(corners) == 0:
-        return None
-
-    try:
-        ret, c_corners, c_ids = cv2.aruco.interpolateCornersCharuco(corners,
-                                                                    ids,
-                                                                    frame,
-                                                                    board)
-        ret, p_rvec, p_tvec = cv2.aruco.estimatePoseCharucoBoard(c_corners,
-                                                                c_ids,
-                                                                board,
-                                                                camera_matrix,
-                                                                dist_coeffs)
-        if p_rvec is None or p_tvec is None:
-            return None
-        if np.isnan(p_rvec).any() or np.isnan(p_tvec).any():
-            return None
-        cv2.aruco.drawAxis(frame,
-                        camera_matrix,
-                        dist_coeffs,
-                        p_rvec,
-                        p_tvec,
-                        0.1)
-        # cv2.aruco.drawDetectedCornersCharuco(frame, c_corners, c_ids)
-        # cv2.aruco.drawDetectedMarkers(frame, corners, ids)
-        # cv2.aruco.drawDetectedMarkers(frame, rejected_points, borderColor=(100, 0, 240))
-    except cv2.error:
-        return None
-
-    if verbose:
-        print('Translation : {0}'.format(p_tvec))
-        print('Rotation    : {0}'.format(p_rvec))
-        print('Distance from camera: {0} m'.format(np.linalg.norm(p_tvec)))
-
-    return frame
 
 if __name__ == '__main__':
-
 
     initCamera()
 
@@ -72,11 +27,7 @@ if __name__ == '__main__':
     marker_length = 0.01625
     board = aruco.CharucoBoard_create(squares_x, squares_y, square_length, marker_length, dictionary)
 
-    layout = [
-        [ sg.Button('Add'), sg.Button('Finish'), sg.Button('Close') ]
-    ]
-
-    window = sg.Window('marker', layout, disable_minimize=True, element_justification='c')
+    total_frame_count = 50
 
     all_corners = []
     all_ids = []
@@ -86,63 +37,61 @@ if __name__ == '__main__':
 
     last_capture = time.time()
     while True:
-        event, values = window.read(timeout=1)
 
-        if event == sg.WIN_CLOSED or event == 'Close':
+        frame = getCameraFrame()
 
-            closeCamera()
-            break
-        
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        imsize = gray.shape
+
+        corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(gray, dictionary)
+
+        if len(corners) > 0:
+            ret, c_corners, c_ids = cv2.aruco.interpolateCornersCharuco(corners, ids, gray, board)
+            # ret is the number of detected corners
+            if ret > 0:
+
+                aruco.drawDetectedCornersCharuco(frame, c_corners, c_ids, (255, 0, 0))
+                if len(c_corners) == (squares_x - 1) * (squares_y - 1) and 1 <= time.time() - last_capture:
+                    all_corners.append(c_corners)
+                    all_ids.append(c_ids)
+
+                    print(f'frame count {len(all_corners)}/{total_frame_count}')
+
+                    last_capture = time.time()
+
+                    if len(all_corners) == total_frame_count:
+
+                        print('calibrating camera. It may take several tens of seconds.')
+                        ret, camera_matrix, dist_coeffs, rvec, tvec = cv2.aruco.calibrateCameraCharuco(
+                            all_corners, all_ids, board, imsize, None, None
+                        )
+
+                        params = read_params()
+                        params['cameras'][dictionary_name] = {
+                            "camera-matrix": camera_matrix.tolist(),
+                            "dist-coeffs": dist_coeffs.tolist()
+                        }
+
+                        write_params(params)
+                        print(f'camera calibration completed. time:{time.time() - last_capture:.1f}sec')
+                        break
+
+            if c_ids is not None:
+                cv2.putText(frame, text=f'detected markers:{len(c_ids)}', org=(5, 25),
+                    fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                    fontScale=0.8,
+                    color=(0, 0, 255),
+                    thickness=1,
+                    lineType=cv2.LINE_AA
+                )
+
         else:
-
-            frame = getCameraFrame()
-
-            if camera_matrix is None:
-                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                imsize = gray.shape
-
-                corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(gray, dictionary)
-
-                if len(corners) > 0:
-                    ret, c_corners, c_ids = cv2.aruco.interpolateCornersCharuco(corners, ids, gray, board)
-                    # ret is the number of detected corners
-                    if ret > 0:
-
-                        aruco.drawDetectedCornersCharuco(frame, c_corners, c_ids, (255, 0, 0))
-                        if len(c_corners) == (squares_x - 1) * (squares_y - 1) and 0.5 <= time.time() - last_capture:
-                            all_corners.append(c_corners)
-                            all_ids.append(c_ids)
-
-                            print(len(c_corners), len(c_ids), len(all_corners))
-
-                            last_capture = time.time()
-
-                            if len(all_corners) == 50:
-                                print('calibrate camera')
-                                ret, camera_matrix, dist_coeffs, rvec, tvec = cv2.aruco.calibrateCameraCharuco(
-                                    all_corners, all_ids, board, imsize, None, None
-                                )
-
-                                params = read_params()
-                                params['cameras'][dictionary_name] = {
-                                    "camera-matrix": camera_matrix.tolist(),
-                                    "dist-coeffs": dist_coeffs.tolist()
-                                }
-
-                                write_params(params)
-                                print('write params')
-                                break
+            c_corners   = None
+            c_ids       = None
 
 
-
-                else:
-                    c_corners   = None
-                    c_ids       = None
-
-            else:
-                draw_axis(frame, camera_matrix, dist_coeffs, board, False)
-
-
-            cv2.imshow("camera", frame)
+        cv2.imshow("camera", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
     closeCamera()    
